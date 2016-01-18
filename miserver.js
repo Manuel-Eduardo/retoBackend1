a24ccdc47849141ec9476130931ed0b03c75dc1e
Manuel <eedduuaarrdo.ce@gmail.com>
2016-01-17 21:00:01 -0500

    Inicio

---
+++ miserver.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var fk = require('faker');
+var ld = require('lodash');
+var app = express();
+
+//Mi funcion 
+
+var generarUsuario = function(){
+
+	var usuario = {
+			id : fk.random.uuid(),
+			userName : fk.internet.userName(),
+			contraseña : fk.internet.password(),
+			nombre : fk.name.findName(),
+			email : fk.internet.email(),
+			imagen : fk.internet.avatar(),
+			telefono : fk.phone.phoneNumberFormat(),
+			pais : fk.address.country(),
+			ciudad : fk.address.city(),	
+	}	
+
+	return usuario;
+}
+
+var generarPost = function(){
+
+	var post = {
+			id : fk.random.uuid(),
+			userName : fk.internet.userName(),
+			imagen : fk.internet.avatar(),
+			pais : fk.address.country(),
+			contenido : fk.lorem.paragraph(),
+			fecha : fk.date.future(),
+			url : fk.internet.url()
+
+	}	
+
+	return post;
+}
+
+
+app.get('/', function (req,res){
+	res.send('Mi primer servidor!');
+})
+
+
+app.get('/usuarios', function (req,res){
+	var cantidad = ld.random(5,10);
+	var usuarios = ld.times(cantidad, generarUsuario);
+
+	res.json(usuarios);
+})
+
+app.get('/Posts', function (req,res){
+
+	var cantidad = ld.random(5,10);
+	var posts = ld.times(cantidad, generarPost);
+
+	res.json(posts);
+})
+
+
+app.listen(3000);
